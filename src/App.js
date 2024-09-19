8d4ef5a8e75ac7941c36016eff12d59a63d42aa7
Yashvardhan Goel <85873488+GOAT0P@users.noreply.github.com>
2024-09-19 19:35:42 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AmbassadorForm from './AmbassadorForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AmbassadorForm />
+    </div>
+  );
+}
+
+export default App;
